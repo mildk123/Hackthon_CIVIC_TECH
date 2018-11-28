6fdd261f0e722628172aa6de021bd85abdc56764
mildk123 <mildk123@gmail.com>
2018-11-28 22:57:14 +0500

    abc

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
-import { 
+import {
   StyleSheet
 } from "react-native";
+
+import { Font, AppLoading } from "expo";
+
 import AppStackNavigator from './navigation/navigator'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
 
+
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+
+      await Font.loadAsync({
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+      })
+    ]);
+  };
 
   render() {
+    if (this.state.loading) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onFinish={() => this.setState({ loading: false }) }
+        />
+      );
+    }
     return (
       <AppStackNavigator />
     );
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,21 +1,58 @@
-import React from "react";
-import { 
-    View,
-    Text,
-    StyleSheet
-} from "react-native";
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import { Container, Content, Form, Item, Label, Input, Button, Text } from 'native-base'
 
-const LoginScreen = (props) => (
-    <View style={styles.container}>
-        <Text>LoginScreen</Text>
-    </View>
-    )
+import firebase from '../Config/firebase'
+const database = firebase.database().ref()
+
+class LoginScreen extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+
+    _signIn = () => {
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch((error) => {
+                alert(error.message)
+            });
+    }
+
+    _onChange = (key, event) => {
+        console.log(key, event )
+    }
+
+    render() {
+        return (
+            <Container>
+                <Content>
+                    <Form>
+                        <Item floatingLabel>
+                            <Label>Username</Label>
+                            <Input onChange={() => this._onChange('Username')} />
+                        </Item>
+                        <Item floatingLabel>
+                            <Label>Password</Label>
+                            <Input onChange={() => this._onChange('Password')} />
+                        </Item>
+                    </Form>
+                    <Button info >
+                        <Text> Sign In </Text>
+                    </Button>
+                </Content>
+            </Container>
+        );
+    }
+}
 export default LoginScreen;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'space-evenly'
     }
 });
