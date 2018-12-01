23f1d820bc0de14d30a4dc0a16b1ff6a3459675c
mildk123 <mildk123@gmail.com>
2018-12-02 03:42:03 +0500

    Added Map For Driver

--- screens/mapScreen.js
+++ screens/mapScreen.js
@@ -1,16 +1,58 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
 
 export default class LinksScreen extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      region: {
+        latitude: 24.918266,
+        longitude: 67.102720,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      markers: [{
+        latitude: 24.918266,
+        longitude: 67.102720,
+        title: 'My Marker',
+        description: 'Here is my marker'
+      }]
+    }
+  }
+
   static navigationOptions = {
     title: 'Map',
   };
 
+
+
+  onRegionChange = (region) => {
+    let text = region
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-          <Text>Map here</Text>
-      </View>
+
+        <MapView
+          style={styles.Map}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        >
+          {this.state.markers.map(marker => (
+            <Marker
+              key={marker.title}
+              coordinate={{
+                latitude: marker.latitude,
+                longitude: marker.longitude
+              }}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+        </MapView>
     );
   }
 }
@@ -20,4 +62,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  Map: {
+    position: 'absolute',
+    top: 0, 
+    left: 0,
+    bottom: 0,
+    right: 0,
+  }
 });
