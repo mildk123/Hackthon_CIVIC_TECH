2d9b7e61d321446c549316ad2c838c5b051045c1
mildk123 <mildk123@gmail.com>
2018-12-02 06:13:36 +0500

    ADDED STUDENT LIST

--- screens/studentList.js
+++ screens/studentList.js
@@ -1,28 +1,97 @@
 import React from 'react';
 import {
-  ScrollView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 
+import { Container, Content, List, ListItem, Left, Body, Right, Thumbnail, Button, Spinner } from 'native-base';
+import ShowMore from '../components/showMore';
+
+
 export default class HomeScreen extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoaded: false,
+      isLoading: true,
+      kidlist: [],
+      selectedKid: null
+    }
+
+  }
+
   static navigationOptions = {
-    title : 'Students '
+    title: 'Students '
   };
 
+
+  componentDidMount = () => {
+    fetch('https://randomuser.me/api/?results=45')
+      .then(call => call.json())
+      .then(data => this.setState({
+        kidlist: data.results,
+        isLoaded: true,
+        isLoading: false
+      })
+      )
+      .catch(err => console.log(err))
+  }
+
+
+  _viewMore = (index) => {
+    this.setState({
+      showMore: true,
+      selectedKid: this.state.kidlist[index]
+    })
+  }
+
+  _back = () => {
+    this.setState({
+      isLoaded: true,
+      isLoading: false,
+      showMore: false,
+    })
+  }
+
   render() {
+    const { isLoading, isLoaded, kidlist, showMore } = this.state;
+    if (isLoading) return <Spinner />
+    if (showMore) return <ShowMore selectedKid={this.state.selectedKid} goBack={this._back} />
+    if (!isLoaded || !kidlist) return null;
     return (
       <View style={styles.container}>
-        <ScrollView>
-          <Text>Students List here</Text>
-        </ScrollView>
+        <Container>
+          <Content>
+            <List>
+              {kidlist && kidlist.map((kid, index) => {
+                return <ListItem
+                  avatar
+                  onPress={() => this._viewMore(index)}
+                  key={index}
+                >
+                  <Left>
+                    <Thumbnail source={{ uri: kid.picture.thumbnail }} />
+                  </Left>
+                  <Body>
+                    <Text>{kid.name.first} {kid.name.last}</Text>
+                    <Text note>Doing what you like will always</Text>
+                  </Body>
+                  <Right>
+                    <Text note>3:43 pm</Text>
+                  </Right>
+                </ListItem>
+              })
+              }
+            </List>
+          </Content>
+        </Container>
 
       </View>
     );
   }
 
- 
+
 
 }
 
